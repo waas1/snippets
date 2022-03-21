73facbce017d6a1cd46ef2bf281ed87f4269804a
Erfan Ilyas <repo-bot@waas1.com>
2022-03-21 12:51:10 +0500

    updated

--- woo-redirect-on-successful-payment.php
+++ woo-redirect-on-successful-payment.php
@@ -9,13 +9,49 @@ add_action( 'woocommerce_thankyou', 'waas1_woocommerce_thankyou_redirect');
   
 function waas1_woocommerce_thankyou_redirect( $orderId ){
 	
+	$conditionCatSlugs 			= array( 'templates' ); //add all product categories where you want to run this snippet.
+	$currentProductCat			= ''; //keep it empty
 	$thankYouPageSlug 			= 'thank-you';
 	$renewalThankYouPageSlug 	= 'renewal-thank-you';
 	
+	
+	
 	$order 	= wc_get_order( $orderId );
 	
-	
+
 	if ( !$order->has_status('failed') ) {
+		
+		
+		$items = $order->get_items();
+		$processCustomRedirect = false;
+		
+		foreach ( $items as $item ) {
+			
+			$productId = $item->get_product_id();
+			$allCats = get_the_terms( $productId, 'product_cat' );
+			
+			if( empty($allCats) ){
+				return false;
+			}
+			
+			foreach( $allCats as $cat ){
+				foreach( $conditionCatSlugs as $conditionCatSlug ){
+					if( $cat->slug == $conditionCatSlug ){
+						$processCustomRedirect = true;
+						$currentProductCat = $cat->slug;
+						break;
+					}
+				}
+			}
+			
+		}//endforeach
+
+		
+		
+		if( !$processCustomRedirect ){
+			return false;
+		}
+		
 		
 		$subscriptions = wcs_get_subscriptions_for_order( $orderId, array('order_type'=>'any') );
 
@@ -25,9 +61,9 @@ function waas1_woocommerce_thankyou_redirect( $orderId ){
 		}
 		
 		if( $orderId == $parentOrderId ){
-			$url = get_home_url().'/'.$thankYouPageSlug.'/?unique-order-id='.$orderId;
+			$url = get_home_url().'/'.$thankYouPageSlug.'-'.$currentProductCat.'/?unique-order-id='.$orderId;
 		}else{
-			$url = get_home_url().'/'.$renewalThankYouPageSlug.'/?parent-order-id='.$orderId;
+			$url = get_home_url().'/'.$renewalThankYouPageSlug.'-'.$currentProductCat.'/?parent-order-id='.$orderId;
 		}
 		
 		wp_safe_redirect( $url );
